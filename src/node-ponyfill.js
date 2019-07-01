2864f4fa135c00adaae3a646abbc4677e241191d
root <root@botstack-grid.wagerstacks.com>
2019-07-01 03:48:57 +0000

    proxy-changes

--- src/node-ponyfill.js
+++ src/node-ponyfill.js
@@ -1,13 +1,31 @@
 var nodeFetch = require('node-fetch')
 var realFetch = nodeFetch.default || nodeFetch
+var wrappedFetch = require('socks5-node-fetch')
+
 
 var fetch = function (url, options) {
   // Support schemaless URIs on the server for parity with the browser.
   // Ex: //github.com/ -> https://github.com/
   if (/^\/\//.test(url)) {
-    url = 'https:' + url
+  	url = 'https:' + url
   }
   return realFetch.call(this, url, options)
+}
+
+
+
+
+if(process.env.SOCKS_PROXY!="" && process.env.SOCKS_PORT!=""){
+	//We know we are using a Proxy System
+
+	console.log("Fetch with Proxy Support Loaded....");
+	var fetch = wrappedFetch({
+		socksHost: process.env.SOCKS_PROXY,
+		socksPort: process.env.SOCKS_PORT
+	});
+
+	fetch.call(this, url, options);
+
 }
 
 module.exports = exports = fetch
